541aabc493347c9cecfde5945a92fe63ca7f1e90
stranik87 <tulkin.boynazarov@gmail.com>
2023-05-17 13:11:30 +0500

    name done

--- name.py
+++ name.py
@@ -1,5 +1,6 @@
 import requests
 from randommer import Randommer
+from pprint import pprint
 
 
 class Name(Randommer):
@@ -14,7 +15,20 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        endpoint = 'Name'
+        url = self.get_url() + endpoint
+        header = {
+            'X-Api-Key':api_key
+        }
+        payload = {
+            'nameType':nameType,
+            'quantity':quantity
+        }
+        res = requests.get(url=url,headers=header,params=payload)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return False
     
     def get_name_suggestions(self, api_key: str, startingWords: str) -> list:
         '''get name suggestions
@@ -26,7 +40,19 @@ class Name(Randommer):
         Returns:
             list: list of name suggestions
         '''
-        pass
+        endpoint = 'Name/Suggestions'
+        url = self.get_url() + endpoint
+        header = {
+            'X-Api-Key':api_key
+        }
+        payload = {
+            'startingWords':startingWords
+        }
+        res = requests.get(url=url,headers=header,params=payload)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return False
     
     def get_name_cultures(self, api_key: str) -> list:
         '''get available cultures
@@ -37,4 +63,17 @@ class Name(Randommer):
         Returns:
             list: list of names
         '''
-        pass
+        endpoint = 'Name/Cultures'
+        url = self.get_url() + endpoint
+        header = {
+             'X-Api-Key':api_key
+        }
+        res = requests.get(url=url,headers=header)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return None
+
+nm = Name()
+
+pprint(nm.get_name_cultures('8a2128f0e8254c4c8bbe8561b12f88c0'))
