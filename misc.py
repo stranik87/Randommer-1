ddfe1dceb50cd82d0d4c09e7dc0f7c3f98555f35
stranik87 <tulkin.boynazarov@gmail.com>
2023-05-17 08:26:26 +0300

    misc

--- misc.py
+++ misc.py
@@ -12,7 +12,16 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
+        endpoint = 'Misc/Cultures'
+        url = self.get_url() + endpoint
+        header = {
+            'X-Api-Key':api_key
+        }
+        res = requests.get(url=url,headers=header)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return None
     
     def get_random_address(self, api_key: str, number: int, culture='en') -> list:
         '''get available misc cultures
@@ -25,4 +34,20 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        endpoint = 'Misc/Random-Address'
+        url = self.get_url() + endpoint
+        headers = {
+            'X-Api-Key':api_key
+        }
+        payload = {
+            'number':number
+        }
+        res = requests.get(url=url,headers=headers,params=payload)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return False
+
+ms = Misc()
+
+print(ms.get_random_address('8a2128f0e8254c4c8bbe8561b12f88c0',2))
