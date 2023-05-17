b171c492fd28a4dcee46c55a20d82c0e99f90d38
stranik87 <tulkin.boynazarov@gmail.com>
2023-05-17 08:09:31 +0300

    done

--- finance.py
+++ finance.py
@@ -56,7 +56,17 @@ class Finance(Randommer):
         Returns:
             list: crypto address
         '''
-        pass
+        endpoint = 'Finance/Countries'
+        url = self.get_url() + endpoint
+        headers = {
+            'X-Api-Key': api_key
+                
+        }
+        res = requests.get(url=url,headers=headers)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return False
 
     def get_iban_by_country_code(self, country_code: str, api_key: str) -> dict:
         '''get available countries
@@ -68,8 +78,22 @@ class Finance(Randommer):
         Returns:
             dict: idan data
         '''
+        endpoint = 'Finance/Iban/ad'
+        url = self.get_url() + endpoint
+        payload = {
+            'countryCode':country_code
+        }
+        headers = {
+            'X-Api-Key': api_key
+                
+        }
+        res = requests.get(url=url,params=payload, headers=headers)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return False
        
 
 fin = Finance()
 
-print(fin.get_crypto_address('Polis','8a2128f0e8254c4c8bbe8561b12f88c0'))
+print(fin.get_iban_by_country_code('AE','8a2128f0e8254c4c8bbe8561b12f88c0'))
